2786a39ed3d338ea781190c9bbefc7306b7aeb6d
WandersonAlves <wandersonalvesferreira2012@gmail.com>
2018-05-26 14:17:31 -0300

    feat(about): WIP about section

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <presentation></presentation>
+    <about></about>
   </div>
 </template>
 
 <script>
 import Presentation from './views/Presentation.vue';
+import About from './views/About.vue';
 
 export default {
   name: 'app',
   components: {
-    Presentation
+    Presentation,
+    About
   }
 }
 </script>
---
+++ src/components/AboutInfo.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="flex flex-column">
+        <h3 class="font-montserrat">{{title}}</h3>
+        <p>{{value}}</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'AboutInfo',
+    props: ['title', 'value']
+}
+</script>
+<style lang="scss" scoped>
+h3 {
+    font-weight: 500;
+    font-size: 24px;
+    color: #e64980;
+    text-transform: uppercase;
+}
+p {
+    font-size: 1em;
+    font-weight: 300;
+}
+</style>
+
+
